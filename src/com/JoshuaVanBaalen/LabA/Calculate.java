05ed2dc2af1bda64442d60b2778423843708a4ac
Joshua Van Baalen <joshuavanbaalen@gmail.com>
2020-10-12 11:47:45 -0600

    Created packages and classes, created base methods for Calculate

---
+++ src/com/JoshuaVanBaalen/LabA/Calculate.java
@@ -0,0 +1,5 @@
+package com.JoshuaVanBaalen.LabA;
+
+public class Calculate {
+
+}
---
+++ src/com/JoshuaVanBaalen/LabA/Main.java
@@ -0,0 +1,10 @@
+package com.JoshuaVanBaalen.LabA;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
